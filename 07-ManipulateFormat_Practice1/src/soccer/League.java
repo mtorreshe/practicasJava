1579fd531e48b4d114dce772be2b2cd36df414bf
Marco Torres <mtorreshe@ipn.mx>
2022-12-06 19:52:24 -0600

    Practica 7

---
+++ 07-ManipulateFormat_Practice1/src/soccer/League.java
@@ -0,0 +1,82 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package soccer;
+
+/**
+ *
+ * @author Administrator
+ */
+public class League {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        // Create team1      
+        Player player1 = new Player();
+        player1.playerName = "George Eliot";
+        Player player2 = new Player();
+        player2.playerName = "Graham Greene";
+        Player player3 = new Player();
+        player3.playerName = "Geoffrey Chaucer";
+        Player[] thePlayers = {player1, player2, player3 };
+        
+        Team team1 = new Team();
+        team1.teamName = "The Greens";
+        team1.playerArray = thePlayers;
+        
+        // Create team2
+        Team team2 = new Team();
+        team2.teamName = "The Reds";
+        team2.playerArray = new Player[3];
+        team2.playerArray[0] = new Player();
+        team2.playerArray[0].playerName = "Robert Service";
+        team2.playerArray[1] = new Player();
+        team2.playerArray[1].playerName = "Robbie Burns";
+        team2.playerArray[2] = new Player();
+        team2.playerArray[2].playerName = "Rafael Sabatini";
+        
+        Game currGame = new Game();
+        currGame.homeTeam = team1;
+        currGame.awayTeam = team2;
+        Goal goal1 = new Goal();
+        goal1.thePlayer = currGame.homeTeam.playerArray[2];
+        goal1.theTeam = currGame.homeTeam;
+        goal1.theTime = 55;
+        Goal[] theGoals = {goal1};
+        currGame.goals = theGoals;
+        
+        System.out.println("Goal scored after " + 
+                currGame.goals[0].theTime + " mins by " +
+                currGame.goals[0].thePlayer.playerName + " of " +
+                currGame.goals[0].theTeam.teamName);
+        
+        /* Practice 7-1. Add code for finding a player within team2 here */
+        
+        for(Player jugador:team2.playerArray){
+            if( jugador.playerName.contains("Sab")){
+                System.out.println("Se encontro al jugador "+jugador.playerName);
+                
+                String[] nombres = jugador.playerName.split(" ");
+                System.out.println("Su apellido es"+nombres[1]);
+                
+            }
+        }
+        
+        for(Player jugador:team2.playerArray){
+             String[] nombres = jugador.playerName.split(" ");
+             
+             StringBuilder apellidoPrimero = new StringBuilder();
+             apellidoPrimero.append(nombres[1]+", "+nombres[0]);
+             
+             System.out.println(apellidoPrimero);
+        }
+        
+        
+    }   
+}
